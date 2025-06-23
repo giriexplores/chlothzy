d0866ba74cdd2054ac2d663b2eef5fb79f35e183
Giri - Aditya Kumar <aditkumar0715@gmail.com>
2025-06-23 11:53:39 +0000

    update the cookie set option to sameSite:'none'

--- chlothzy_backend/controllers/user.controller.js
+++ chlothzy_backend/controllers/user.controller.js
@@ -92,6 +92,7 @@ export const loginUser = async (req, res) => {
       expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
       httpOnly: true,
       secure: true,
+      sameSite: 'none',
     };
     res.cookie('token', token, options).status(200).json({
       success: true,
@@ -111,6 +112,7 @@ export const logoutUser = (req, res) => {
     const options = {
       httpOnly: true,
       secure: true,
+      sameSite: 'none',
     };
     return res.status(200).clearCookie('token', options).json({
       success: true,
